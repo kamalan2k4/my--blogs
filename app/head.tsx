b32ffbe0b3e0cfa695df4ebdbbccccbfe0bcf99b
kamalan2k4 <kmln2k4@gmail.com>
2024-11-10 18:31:08 +0530

    second commit

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Jack's Blog</title>
+      <title>kamal's Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon"  className="rounded-full" href="/profile.jpg" />
     </>
